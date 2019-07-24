00e037100610907a13b0b5a3f38ce64b13e42c8c
tony <tony950620@naver.com>
2019-07-24 18:01:19 +0900

    20190724

--- SpringBootToad/src/main/java/com/Spring/User/controller/main.java
+++ SpringBootToad/src/main/java/com/Spring/User/controller/main.java
@@ -3,6 +3,9 @@ package com.Spring.User.controller;
 import java.sql.SQLException;
 import java.util.Random;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.Spring.User.dao.DaoFactory;
 import com.Spring.User.dao.UserDao;
 import com.Spring.User.vo.User;
@@ -10,15 +13,16 @@ import com.Spring.User.vo.User;
 public class main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao userdao = new DaoFactory().userDao();
-
+		//UserDao userdao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao userdao = context.getBean("userDao", UserDao.class);
+		
 		User user = new User();
 		user.setId("" + new Random().nextInt(10000));
 		user.setName("태현공");
 		user.setPassword("123");
 		
 		userdao.add(user);
-		
 		System.out.println("결과2 : " + userdao.get(user.getId()));
 		
 	}
--- SpringBootToad/src/main/java/com/Spring/User/dao/DaoFactory.java
+++ SpringBootToad/src/main/java/com/Spring/User/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package com.Spring.User.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
 	
+	@Bean
 	public UserDao userDao(){
 		ConnectionMaker a = connectionMaker();
 		return new UserDao(a);
